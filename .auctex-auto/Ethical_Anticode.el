2323d486d11bab3707ecc37c4b15a60c4b7ca338
Givingdonation <gitlab@iscienceworld.com>
2024-10-13 15:30:34 -0700

    schizo and 2 paras to anticode

--- .auctex-auto/Ethical_Anticode.el
+++ .auctex-auto/Ethical_Anticode.el
@@ -7,6 +7,13 @@
                      '(("article" "letter")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("geometry" "margin=1.5cm") ("hyperref" "") ("graphicx" "")))
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
     "article"
